044423a6043514d97b08e8f6aca01e501b90f78a
BIBI <dingdongx2@hanyang.ac.kr>
2018-11-04 23:04:26 +0900

    edit) model opt

--- makeModel.py
+++ makeModel.py
@@ -13,8 +13,8 @@ img_w, img_h = 150, 150
 train_data_dir = './dataset/training_set'
 test_data_dir = './dataset/test_set'
 nb_train_samples = 100
-nb_test_samples = 20
-epochs = 10
+nb_test_samples = 50
+epochs = 25
 batch_size = 16
 
 if K.image_data_format() == 'channels_first':
@@ -39,7 +39,7 @@ train_generator = train_datagen.flow_from_directory(
         train_data_dir, # img 경로
         target_size=(img_w, img_h), # 패치 이미지 크기
         batch_size=batch_size, # 배치 크기
-        class_mode='binary') # categorical/binary/sparse/None
+        class_mode='categorical') # categorical/binary/sparse/None
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 # 검증용 generator 생성
@@ -47,32 +47,27 @@ test_generator = test_datagen.flow_from_directory(
         test_data_dir,
         target_size=(img_w, img_h),
         batch_size=batch_size,
-        class_mode='binary')
+        class_mode='categorical')
 
 
 
 # 2. 모델 구성하기
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=input_shape, activation='relu'))
 # model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Dense(64, activation='relu'))
 # model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Dense(3, activation='sigmoid'))
 
 # 3. 모델 학습과정 설정하기
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -84,7 +79,7 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 # 4. 모델 학습시키기
 model.fit_generator(train_generator, steps_per_epoch=nb_train_samples, epochs=epochs, validation_data=test_generator, validation_steps=nb_test_samples)
-model.save_weights('first_model.h5')
+model.save_weights('test_model.h5')
 
 # 5. 모델 평가하기
 print("-- Evaluate(정확도) --")
